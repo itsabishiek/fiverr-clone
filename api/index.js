1491ab2caf757759526743884b7255fa168e742e
User <s.abishiek007@gmail.com>
2023-04-02 21:19:27 +0530

    API updated

--- api/index.js
+++ api/index.js
@@ -26,7 +26,7 @@ const connect = async () => {
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: "https://fiverrrrr.vercel.app", credentials: true }));
 
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
